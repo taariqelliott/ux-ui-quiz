6f95ad34c602d625e2ca75ba4b9bfe051c6ea65c
Taariq Elliott <taariq.elliott@yahoo.com>
2023-08-20 18:18:36 -0700

    added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import emailjs from "@emailjs/browser";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "../components/Contact.css";
-import './toast.css'
+import "../components/Toast.css"
 
 const Contact = () => {
   const form = useRef();
